1b58a1db3d99a3dd79428782a3c3851e608a6fee
Itai Fish <chessandpotatoes@gmail.com>
2025-08-16 08:32:40 -0400

    tabs are better

--- problem12.py
+++ problem12.py
@@ -6,27 +6,27 @@ prime_factors_dict: Dict[int, List[int]] = {}
 
 
 def prime_factors_for_num(number: int):
-    if prime_factors_dict.get(number) != None:
-        return prime_factors_dict[number]
-    factors = []
-    finished = False
-    cur_number = number
-    while not finished:
-        i = 2
-        max = sqrt(cur_number)
-        while i <= max:
-            result, remainder = divmod(cur_number, i)
-            if remainder == 0:
-                factors += prime_factors_for_num(i)
-                cur_number = int(result)
-                break
-            i += 1
-        if i >= max:
-            finished = True
-    prime_factors_dict[cur_number] = [cur_number]
-    factors.append(cur_number)
-    prime_factors_dict[number] = factors
-    return factors
+	if prime_factors_dict.get(number) != None:
+		return prime_factors_dict[number]
+	factors = []
+	finished = False
+	cur_number = number
+	while not finished:
+		i = 2
+		max = sqrt(cur_number)
+		while i <= max:
+			result, remainder = divmod(cur_number, i)
+			if remainder == 0:
+				factors += prime_factors_for_num(i)
+				cur_number = int(result)
+				break
+			i += 1
+		if i >= max:
+			finished = True
+	prime_factors_dict[cur_number] = [cur_number]
+	factors.append(cur_number)
+	prime_factors_dict[number] = factors
+	return factors
 
 
 # essentially what we're doing here is splitting this into
@@ -37,31 +37,31 @@ def prime_factors_for_num(number: int):
 # 2*2*2 etc equal to the number of prime factors, which is the number of unique combinations
 # without repetition
 def count_divisors_from_prime_factors(prime_factors: List[int]):
-    sorted_factors = sorted(prime_factors)
-    last_num = -1
-    running_total_count = 0
-    running_total_divisors = 1
-    for factor in sorted_factors:
-        if factor == last_num:
-            running_total_count += 1
-        else:
-            running_total_divisors *= running_total_count + 1
-            running_total_count = 1
-            last_num = factor
-    running_total_divisors *= running_total_count + 1
-    return running_total_divisors
+	sorted_factors = sorted(prime_factors)
+	last_num = -1
+	running_total_count = 0
+	running_total_divisors = 1
+	for factor in sorted_factors:
+		if factor == last_num:
+			running_total_count += 1
+		else:
+			running_total_divisors *= running_total_count + 1
+			running_total_count = 1
+			last_num = factor
+	running_total_divisors *= running_total_count + 1
+	return running_total_divisors
 
 
 def first_triangle_number_with_divisors(num_divisors_min: int):
-    triangle_number = 0
-    position = 0
-    while True:
-        position += 1
-        triangle_number += position
-        prime_factors = prime_factors_for_num(triangle_number)
-        divisors = count_divisors_from_prime_factors(prime_factors)
-        if divisors >= num_divisors_min:
-            return triangle_number
+	triangle_number = 0
+	position = 0
+	while True:
+		position += 1
+		triangle_number += position
+		prime_factors = prime_factors_for_num(triangle_number)
+		divisors = count_divisors_from_prime_factors(prime_factors)
+		if divisors >= num_divisors_min:
+			return triangle_number
 
 
 print(first_triangle_number_with_divisors(501))
