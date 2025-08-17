83aa69db49a38af64d51e3576d7d366dad38ded0
Itai Fish <chessandpotatoes@gmail.com>
2025-08-17 09:58:54 -0400

    updates

--- runner.py
+++ runner.py
@@ -1,10 +1,11 @@
 import sys
 import os
 from pathlib import Path
+import timeit
 import subprocess
 import statistics
 
-FILES_DIR = Path("/euler-solutions")
+FILES_DIR = Path("euler-solutions")
 
 if len(sys.argv) != 2:
 	print(f"Usage: {sys.argv[0]} <num_runs>")
@@ -14,9 +15,9 @@ num_runs = int(sys.argv[1])
 
 # mapping from filename -> [runtimes]
 runtimes = {}
-
-print(f"Running input files {num_runs} times to collect statistics")
-for file in os.listdir(FILES_DIR):
+files = os.listdir(FILES_DIR)
+print(f"Running {len(files)} input files {num_runs} times to collect statistics")
+for file in files:
 	file_path = FILES_DIR / Path(file)
 
 	if file_path.suffix != ".py":
@@ -24,13 +25,10 @@ for file in os.listdir(FILES_DIR):
 		exit(-2)
 
 	for _ in range(0, num_runs):
-		result = subprocess.run(["python", file_path], capture_output=True, check=True)
-
-		last_line = result.stdout.strip().split(b"\n")[-1]
-		last_line_int = float(last_line)
-
+		time = timeit.timeit(lambda : subprocess.run(["python", file_path], capture_output=True, check=True), number=1)
+		
 		runtimes.setdefault(file, []).append(
-			last_line_int * 1_000
+			time * 1_000
 		)  # convert to milliseconds
 
 runtime_statistics = {
@@ -38,12 +36,12 @@ runtime_statistics = {
 	for filename, runtimes in runtimes.items()
 }
 
-print("Individual Runtime Statistics (min, median, max) in milliseconds")
+print("Individual Runtime Statistics (min, median, max) in miliseconds")
 for filename, filename_statistics in sorted(list(runtime_statistics.items())):
-	print(f"{filename}: {filename_statistics}")
+	print(f"{filename}: {list(map("{:10.2f}".format, filename_statistics))}")
 
 overall_runtime_statistics = tuple(
 	sum(runtimes) for runtimes in zip(*runtime_statistics.values())
 )
-print("Overall Runtime Statistics (min, median, max) in milliseconds")
+print("Overall Runtime Statistics (min, median, max) in milisecondsseconds")
 print(overall_runtime_statistics)
